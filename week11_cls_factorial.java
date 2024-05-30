f59a525ade7481d6442c34e010e30626d6d0990b
tanscoder <149710255+tanscoder@users.noreply.github.com>
2024-05-30 18:37:11 +0530

    Update week11_cls_factorial.java
    factorials both recursive and non recursive done

--- week11_cls_factorial.java
+++ week11_cls_factorial.java
@@ -1,32 +1,31 @@
 public class Main {
-  
-  static  int  i=facto;  
-   static int  factorial=1; 
+
+ 
 
     public static int  factorial(int facto){ 
-      
-     
-    
+
+
+
     if (facto == 1){ 
         return facto;
-       
+
     }
     else { 
 
         facto=facto*factorial(facto-1);
-        
+
 
     }
-                                                     
+
     return facto;                                                                                           
   }
     public static void main(String[] args) {
 
         int factorial=1;
-        for (int i=1;  i<=5;i++ )
-        {factorial = factorial*i;}
+        for (int j=1;  j<=5;j++ )
+        {factorial = factorial*j;}
        System.out.println("factorial:  "+factorial); 
- System.out.print ("factorial by recusive function:  "+ factorial(5)) ;
+ System.out.print ("factorial by recusive function:  "+ factorial(6)) ;
 
     }
 }
