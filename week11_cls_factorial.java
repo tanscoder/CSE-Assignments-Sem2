e4708ee8db03762a381b8ce10a20e2ddc26f7e83
tanscoder <149710255+tanscoder@users.noreply.github.com>
2024-05-30 16:16:38 +0530

    Create week11_cls_factorial.java

---
+++ week11_cls_factorial.java
@@ -0,0 +1,12 @@
+public class Main {
+  
+    public static void main(String[] args) {
+// NON RECURSIVE
+        int factorial=1;
+        for (int i=1;  i<=5;i++ )
+        {factorial = factorial*i;}
+       System.out.println("factorial "+factorial); 
+
+       
+    }
+}
