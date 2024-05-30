a7559a5002d2e9447a39c1ebaf6fcccd7bee34b5
tanscoder <149710255+tanscoder@users.noreply.github.com>
2024-05-30 20:55:33 +0530

    Update week11_cls_factorial.java
    all factorial part done

--- week11_cls_factorial.java
+++ week11_cls_factorial.java
@@ -1,31 +1,36 @@
+import java.util.*;
 public class Main {
 
  
+public static int factorial(int fct){ 
+		   int factorial=1;
+		    for (int j=1;  j<=fct;j++ ){
+                    
+			 factorial = factorial*j;}
+		
+			return factorial; }
 
-    public static int  factorial(int facto){ 
+    public static int  factorialRec(int facto){ 
+                    if (facto == 1){ 
+          			 return facto;}
 
+		    else { 
+		        facto=facto*factorialRec(facto-1);   }
 
+     			    return facto;                                                                                           
+  						}
 
-    if (facto == 1){ 
-        return facto;
-
-    }
-    else { 
-
-        facto=facto*factorial(facto-1);
-
-
-    }
-
-    return facto;                                                                                           
-  }
     public static void main(String[] args) {
 
-        int factorial=1;
-        for (int j=1;  j<=5;j++ )
-        {factorial = factorial*j;}
-       System.out.println("factorial:  "+factorial); 
- System.out.print ("factorial by recusive function:  "+ factorial(6)) ;
+        int factorial=1,n;
+         Scanner sc = new  Scanner (System.in);
+
+	 System.out.println("enter your digit to find the factorial");
+
+	n=sc.nextInt();
+      
+       System.out.println("factorial:by  non recursive  "+factorial(n)); 
+ System.out.print ("factorial by recusive function:  "+ factorialRec(n)) ;
 
     }
 }
